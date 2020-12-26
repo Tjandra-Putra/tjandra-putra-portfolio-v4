cfea468cce673f318d8e3d9f68d4af50b46c71f9
Tjandra Putra <tjandraputra2000@gmail.com>
2020-12-26 22:39:35 +0800

    init: Portfolio Version 5

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// Type.js
+var typed = new Typed(".type", {
+    strings: [
+        "Web Developer.",
+        "Mobile Developer.",
+        "RPA Developer.",
+        "UI/UX Designer."
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+});
