e96c2adced7fc487685ebb2efee21feeaf3431de
Tjandra Putra <tjandraputra2000@gmail.com>
2021-01-25 16:03:56 +0800

    update: sticky nav

--- js/index.js
+++ js/index.js
@@ -10,3 +10,19 @@ var typed = new Typed(".type", {
     backSpeed: 60,
     loop: true
 });
+
+
+if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    document.getElementById("navbar").style.background = "red";
+    console.log(document.body.scrollHeight)
+} else {}
+
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    if (scroll > 700) {
+        document.getElementById("navbar").style.background = "white";
+    } else {
+        document.getElementById("navbar").style.background = "rgb(245, 245, 245)";
+    }
+    console.log(scroll)
+});
