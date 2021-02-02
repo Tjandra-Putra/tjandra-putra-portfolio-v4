9ede375f0a9cf13cda681387afe1efa453594b41
Tjandra Putra <tjandraputra2000@gmail.com>
2021-02-02 22:09:46 +0800

    update: animate on scroll

--- js/index.js
+++ js/index.js
@@ -1,29 +1,27 @@
 // Type.js
-var typed = new Typed(".type", {
-    strings: [
-        "Data Analyst.",
-        "Web Developer.",
-        "Mobile Developer.",
-        "RPA Developer.",
-        "UI/UX Designer."
-    ],
-    typeSpeed: 60,
-    backSpeed: 60,
-    loop: true
+var typed = new Typed('.type', {
+	strings: [ 'Data Analyst.', 'Web Developer.', 'Mobile Developer.', 'RPA Developer.', 'UI/UX Designer.' ],
+	typeSpeed: 60,
+	backSpeed: 60,
+	loop: true
 });
 
-
 if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-    document.getElementById("navbar").style.background = "red";
-    console.log(document.body.scrollHeight)
-} else {}
+	document.getElementById('navbar').style.background = 'red';
+	console.log(document.body.scrollHeight);
+} else {
+}
 
-window.addEventListener("scroll", (event) => {
-    let scroll = this.scrollY;
-    if (scroll > 700) {
-        document.getElementById("navbar").style.background = "white";
-    } else {
-        document.getElementById("navbar").style.background = "rgb(245, 245, 245)";
-    }
-    console.log(scroll)
+window.addEventListener('scroll', (event) => {
+	let scroll = this.scrollY;
+	if (scroll > 700) {
+		document.getElementById('navbar').style.background = 'white';
+	} else {
+		document.getElementById('navbar').style.background = 'rgb(245, 245, 245)';
+	}
+	console.log(scroll);
+});
+
+AOS.init({
+	once: true
 });
