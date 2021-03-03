d2f32a7464792da48d34cc1a401cdce9654e222e
Tjandra Putra <tjandraputra2000@gmail.com>
2021-03-03 12:08:39 +0800

    fixed: bootstrap js

--- js/pointer.js
+++ js/pointer.js
@@ -1,5 +1,5 @@
-/* 
-    pointer.js was created by OwL for use on websites, 
+/*
+    pointer.js was created by OwL for use on websites,
      and can be found at https://seattleowl.com/pointer.
 */
 
